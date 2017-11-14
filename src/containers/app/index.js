3ab2491842615a23a76206fc5f47004389341746
leanrob <rbyrne@xerafloptech.com>
2017-11-14 15:11:16 -0800

    1.1 JSX
    Checking adds comments to the code and readme for section 1.1 about JSX

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -4,7 +4,7 @@ import Home from '../home'
 import About from '../about'
 
 const App = () => (
-  <div>
+  <div className="main">
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
@@ -14,7 +14,19 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
     </main>
+    {/*This is how you comment code in JSX*/}
   </div>
 )
 
 export default App
+
+/**
+ * JSX Introduction
+ * --> JSX is simply HTML-like syntax that is returned by a javascript function
+ * --> Line 6-21 > this case the JS function is an arrow function.
+ * --> Line 23 > This component is then exported for use elsewhere in the app
+ * Note: Line 7-20 > Each JSX component MUST have ONE top level div, otherwise it will rage out
+ * --> Line 17 > see how to comment out JSX code
+ * --> Line 7 > Some elements of HTML syntax need to change and will be converted on render (Such as className) see link [http://www.javascripter.net/faq/reserved.htm]
+ * @constructor
+ */
