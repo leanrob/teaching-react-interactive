dcb6183019f0f68b8c281ffa8ec876e88addaefe
Rob Byrne <rob@leanrob.com>
2017-11-14 22:13:21 -0800

    1.2
    Adding to readme

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 > An interactive tutorial to knowledge share some basic React+Redux knowledge.
 
+## Setting up the project
+
+### Prerequisits
+
+Install the following Chrome extensions..
+
+[https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd]
+[https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi]
+
+### Start the project
+
+To start the project it is as easy as running the one following commend..
+
+`yarn start`
+
+Then opening `localhost:3000` or 3002 is 300 was taken.
+
 ## Chapter 1: React Basics
 
 ### 1.1 JSX
@@ -16,8 +33,10 @@ Pull the 1.1 Tag of the master branch
 
 ### 1.2 Props and State
 
+Pull the 1.2 tag of the master branch
 
-
+--> open `src/containers/about/aboutClassComponent.js`
+--> then open `src/containers/about/aboutUs.js`
 
 ### 1.3 Components
 
--- src/containers/about/aboutClassComponent.js
+++ src/containers/about/aboutClassComponent.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 
 export class AboutUsClassComponent extends Component {
 
-  // This one will show on page load
+	/**
+     * Lifecycle methods fire at different time in the components lifecycly
+     * See this link for a more detailed explanation...
+     * [http://busypeoples.github.io/post/react-component-lifecycle/](Lifecycle Methods)
+	 */
+
+	// This one will show on page load
   componentWillMount() {
+    // Fired just BEFORE the component mounts on the virtual DOM
     console.log('Component will mount');
   }
 
   // This one will show on page load
   componentDidMount() {
+    // Fired just AFTER the component mounts on the virtual DOM
     console.log('Component did mounted');
   }
 
   // This one will NOT show on page load
   shouldComponentUpdate() {
-    console.log('Should Component Mount');
+    console.log('Should component update');
   }
 
   // This one will NOT show on page load
